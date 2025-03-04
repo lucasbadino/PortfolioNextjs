cec94c6a5cd57c3128c80acb88bd54a3501524bf
lucas <lucas.badino.7@gmail.com>
2025-03-04 12:23:44 -0300

    start

---
+++ components/Cover.tsx
@@ -0,0 +1,7 @@
+export const Cover = () => {
+    return (
+        <div>
+            <p>Cover</p>
+        </div>
+    );
+};
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,18 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#f5741c",
+        darkBg: "#131424",
+      },
+      backgruondImage: {
+        "gradient-cover": 
+        "linear-gradient(90.21deg, rgba(70, 54, 124, 0.5) -5.91%, rgba(74, 47, 190, 0.5) 111.58%)",
+      }
+    }
+  }
 };
-
 export default config;
